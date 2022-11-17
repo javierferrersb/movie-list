6102987ad1d105ad4a7139e62d610448a392f6e6
Javier Ferrer Sánchez-Barcaiztegui <javierferrersb@gmail.com>
2022-11-17 15:03:07 +0100

    Initial commit

---
+++ pages/details/[id].tsx
@@ -0,0 +1,15 @@
+import { useRouter } from "next/router";
+
+function DetailsPage() {
+    const router = useRouter();
+    const { id } = router.query;
+
+    return (
+        <div>
+            <h1>Details Page</h1>
+            <p>Id: {id}</p>
+        </div>
+    );
+}
+
+export default DetailsPage;
