0157bf58f1f84b49b33fc9d2ae76ea07d0cc9003
Javier Ferrer Sánchez-Barcaiztegui <javierferrersb@gmail.com>
2022-11-19 20:02:40 +0100

    Solved visual bug in movie card skeleton

--- components/Skeletons/MovieCardSkeleton.tsx
+++ components/Skeletons/MovieCardSkeleton.tsx
@@ -1,10 +1,10 @@
 function MovieCardSkeleton() {
   return (
-    <div className="m-3 flex h-48 animate-pulse flex-row items-center rounded-lg bg-gray-800 shadow-lg">
-      <div className="h-full w-36 rounded-l-xl bg-gray-700"></div>
+    <div className="m-3 flex h-48 animate-pulse flex-row items-center rounded-lg bg-white shadow-xl dark:bg-gray-800">
+      <div className="h-full w-36 rounded-l-xl bg-gray-300 dark:bg-gray-700"></div>
       <div className="ml-1 flex-row p-2 md:ml-5">
-        <div className="h-6 w-32 rounded-lg bg-gray-700"></div>
-        <div className="mt-2 h-6 w-20 rounded-lg bg-gray-700"></div>
+        <div className="h-6 w-32 rounded-lg bg-gray-300 dark:bg-gray-700"></div>
+        <div className="mt-2 h-6 w-20 rounded-lg bg-gray-300 dark:bg-gray-700"></div>
       </div>
     </div>
   );
